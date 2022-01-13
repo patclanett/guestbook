286f6f36b588a12d94eddb462d678cb4ce480be7
tommy <tommy.brumley95@gmail.com>
2022-01-12 18:31:39 -0800

    reagent add

--- src/cljs/guestbook/core.cljs
+++ src/cljs/guestbook/core.cljs
@@ -1,5 +1,11 @@
-(ns guestbook.core)
+(ns guestbook.core
+  (:require [reagent.core :as r]
+            [reagent.dom :as dom]))
 
-(-> (.getElementById js/document "content")
-    (.-innerHTML)
-    (set! "Hello world"))
+;; [:tag-name {:attribute-key "attribute value"} tag body]
+;; <tag-name attribute-key= "attribute value" >tag body</tag-name>
+;; structure of vector representing the HTML nodes
+
+(dom/render
+ [:h1 "success, reagent add"]
+ (.getElementById js/document "content"))
